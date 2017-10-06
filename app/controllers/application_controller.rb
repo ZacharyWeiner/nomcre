73d1578c6110364bc9d1dd7a581714006c6a16ec
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-10-06 10:52:21 -0300

    added theme to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  layout :layout_by_resource
+
    def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :display_name, :profile_image,  :password])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :profile_image,  :password,  :current_password])
   end
+
+   private def layout_by_resource
+      if devise_controller?
+        'layouts/devise'
+      else
+        'layouts/application'
+      end
+    end
 end
