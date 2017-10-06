148fa637ccd4607111ea135df2075fdc82a8a338
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-10-06 11:06:00 -0300

    added route for notificaitons

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :pages
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+  #notifications
+  get '/notifications', to: 'notifications#index', as: 'notifications'
 
   root 'pages#index'
 end
