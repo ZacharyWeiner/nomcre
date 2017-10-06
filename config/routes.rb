9df0d9b31853a75aed446bb21d4b348ddaa69839
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-10-06 14:52:45 -0300

    cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,13 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
   resources :proposals
  # Front routes start
-
   resources :companies
-
   devise_for :users
-
   resources :pages
   resources :chatrooms
   resources :messages
-
   #notifications
   get '/notifications', to: 'notifications#index', as: 'notifications'
-
   #tasks
   resources :tasks do
     get '/complete', to: 'tasks#complete', as: 'task_complete'
