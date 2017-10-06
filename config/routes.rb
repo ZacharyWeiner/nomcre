22f98812ac2d616fc82fef56ec5494a12472bff6
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-10-06 15:39:23 -0300

    forced redirect to create company for usertype-company

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,18 @@ Rails.application.routes.draw do
   resources :collections
   resources :waitlists
   mount ActionCable.server => '/cable'
-  resources :proposals
+
+  resources :proposals do
+    get '/edit_details', to: 'proposals#edit_details', as: 'edit_details'
+    get '/payment', to: 'proposals#payment'
+    get 'create_request/:user_id', to: 'proposals#create_request', as: 'create_request'
+    get 'accept_request', to: 'proposals#accept_request'
+    get 'approve_request', to: 'proposals#approve_request'
+  end
+  get 'proposal/completed', to: 'proposals#completed', as: 'proposals_completed'
+  get 'requests', to: 'proposal_requests#requests', as:'proposal_requests'
+
+
  # Front routes start
   resources :companies
   devise_for :users
