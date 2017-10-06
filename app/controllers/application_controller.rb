4598dfb8cec04c6e98ad1dfd9ad0adec46bea7d5
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-10-06 12:14:24 -0300

    added top menu for logged out state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,40 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :force_set_company
   layout :layout_by_resource
 
+  def home
+    if session[:user_type] == 'creative'
+      current_user.user_type = 'creative'
+      current_user.save
+      session[:user_type] == nil
+    end
+    unless current_user.nil?
+      redirect_to root_path
+    end
+  end
 
+  protected
+  def force_set_company
+    unless current_user.nil? || request.path.include?('session')
+      unless request.path.include?('companies')
+        if (session[:user_type] == "company") || (current_user.user_type == 'company' && current_user.company.nil?)
+          if current_user.user_type.nil? && session[:user_type].nil? == false
+            current_user.user_type = session[:user_type]
+          end
+          if current_user.save
+            session[:user_type] = nil
+            unless current_user.nil? || current_user.user_type != 'company'
+              if current_user.company.nil?
+                return redirect_to new_company_path
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 
    def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :display_name, :profile_image,  :password])
