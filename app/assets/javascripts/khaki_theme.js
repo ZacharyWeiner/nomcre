e91dfb9271d369ad7d4cb38fe5da3b47c8c1b3ea
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-11-05 00:18:29 -0600

    removed less js

--- app/assets/javascripts/khaki_theme.js
+++ app/assets/javascripts/khaki_theme.js
@@ -5,34 +5,7 @@
 //= require jquery-form/dist/jquery.form.min
 //= require jquery-validation/dist/jquery.validate.min
 //= require jquery.countdown/dist/jquery.countdown.min
-//= require gsap/src/minified/TweenMax.min
-//= require gsap/src/minified/plugins/ScrollToPlugin.min
-//= require khaki/jquery.themepunch.enablelog
-//= require khaki/jquery.themepunch.revolution.min
-//= require khaki/jquery.themepunch.tools.min
-//= require tether/dist/js/tether.min
-//= require sticky-kit/dist/sticky-kit.min
-//= require jarallax/dist/jarallax.min
-//= require jarallax/dist/jarallax-video.min
-//= require imagesloaded/imagesloaded.pkgd.min
-//= require flickity/dist/flickity.pkgd.min
-//= require isotope/dist/isotope.pkgd.min
-//= require photoswipe/dist/photoswipe.min
-//= require photoswipe/dist/photoswipe-ui-default.min
-//= require moment/min/moment.min
-//= require moment-timezone/builds/moment-timezone-with-data
-//= require hammer.js/hammer.min
-//= require social-likes/dist/social-likes.min
-//= require nanoscroller/bin/javascripts/jquery.nanoscroller.min
-//= require SoundManager2/script/soundmanager2-nodebug-jsmin
-//= require datetimepicker/build/jquery.datetimepicker.full.min
-//= require keymaster/keymaster
-//= require prism/prism
-//= require khaki/demo
-//= require khaki/khaki-init
-//= require khaki/khaki
-//= require khaki/nk-share
-//= require turbolinks
+
 
 document.addEventListener("turbolinks:load", function() {
 
