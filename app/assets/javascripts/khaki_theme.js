801c4e2307cc6f24c35641a13b9e8e1692b04772
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-11-05 00:19:39 -0600

    removed less js

--- app/assets/javascripts/khaki_theme.js
+++ app/assets/javascripts/khaki_theme.js
@@ -5,6 +5,12 @@
 //= require jquery-form/dist/jquery.form.min
 //= require jquery-validation/dist/jquery.validate.min
 //= require jquery.countdown/dist/jquery.countdown.min
+//= require gsap/src/minified/TweenMax.min
+//= require gsap/src/minified/plugins/ScrollToPlugin.min
+//= require khaki/jquery.themepunch.enablelog
+//= require khaki/jquery.themepunch.revolution.min
+//= require khaki/jquery.themepunch.tools.min
+//= require tether/dist/js/tether.min
 
 
 document.addEventListener("turbolinks:load", function() {
