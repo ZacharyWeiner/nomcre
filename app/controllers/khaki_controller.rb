5b58493a2007ec5b804ca2075224e6d2f9e68e65
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-11-25 15:22:16 -0700

    moved twilio creds to env

--- app/controllers/khaki_controller.rb
+++ app/controllers/khaki_controller.rb
@@ -20,9 +20,9 @@ class KhakiController < ApplicationController
   end
 
   def about
-    @twilio_number = '19545162345'
-    account_sid = 'AC13e2dae35223ccb60942cabc681a1b51'
-    @client = Twilio::REST::Client.new account_sid, '25fb357eb1d1c06b4cf1ba9114bfb290'
+    @twilio_number = ENV['TWILIO_NUMBER']
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    @client = Twilio::REST::Client.new account_sid, ENV['TWILIO_CLIENT_ID']
     time_str = ((Time.now).localtime).strftime("%I:%M%p on %b. %d, %Y")
     reminder = "Hi Zack. Just a reminder that you have an appointment coming up at #{time_str}."
     message = @client.api.account.messages.create(
