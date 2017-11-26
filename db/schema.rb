10f3364dc7e66b461f3b1df9d711d78b341b9605
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-11-26 15:21:42 -0700

    added height & width fields to file upload to preserve original dimensions and anspect ratios for images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171126041210) do
+ActiveRecord::Schema.define(version: 20171126221032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20171126041210) do
     t.string "file"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "height"
+    t.integer "width"
     t.index ["collection_id"], name: "index_collection_items_on_collection_id"
     t.index ["user_id"], name: "index_collection_items_on_user_id"
   end
