a56e6d445b323465692f007a376e324681ce5de4
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-11-25 18:56:47 -0700

    changed dev env to use sendgrid as well as prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,12 +56,13 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.gmail.com',
-  #   port:                 587,
-  #   domain:               'gmail.com',
-  #   user_name:             ENV['GMAIL_USER'],
-  #   password:             ENV['GMAIL_PASS'],
-  #   authentication:       'plain'
-  # }
+   ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'nomcre.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 end
