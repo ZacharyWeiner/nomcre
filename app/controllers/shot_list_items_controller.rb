c36549f8b40dcf3d721e428ad984d4d0f2a64c8e
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2018-02-22 17:54:36 -0700

    updated shot list to take focus point and style if the proposal is 'moxed' and updated the proposal layout to better fit the current info

--- app/controllers/shot_list_items_controller.rb
+++ app/controllers/shot_list_items_controller.rb
@@ -80,6 +80,6 @@ class ShotListItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def shot_list_item_params
-      params.require(:shot_list_item).permit(:proposal_id, :description, :background, :upload, :item_type, :task_id)
+      params.require(:shot_list_item).permit(:proposal_id, :description, :background, :upload, :item_type, :task_id, :focus_point)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221183525) do
+ActiveRecord::Schema.define(version: 20180222224648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -188,6 +188,7 @@ ActiveRecord::Schema.define(version: 20180221183525) do
     t.bigint "task_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "focus_point"
     t.index ["proposal_id"], name: "index_shot_list_items_on_proposal_id"
     t.index ["task_id"], name: "index_shot_list_items_on_task_id"
   end
