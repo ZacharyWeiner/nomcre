c86c708c38e0cf5a9c82e917bdcde48a64f47a67
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2018-03-01 10:06:40 -0700

    moved location select format to location model, changed footers for copyright, removed edit buttons on proposal from creative view, updated location select to use model format on wizard, edit, and schedule items, changed date format on schedule items timeline, and made date selectors the calendar on schedule item form

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,20 @@
 class Location < ApplicationRecord
+  def self.locations_for_select
+    regions  = Location.where(location_type: 'Region')
+    countries = Location.where(location_type: 'Country').collect{|l| [l.name, l.id]}
+    cities = Location.where(location_type: 'City').collect{|l| [l.name, l.id]}
+    options = []
+    regions.each do |region|
+      options << ["#{region.name}", "#{region.id}"]
+      region_countries = Location.where(parent_id: region.id)
+      region_countries.each do |country|
+        options << ["#{region.name} : #{country.name}", "#{country.id}"]
+        country_cities = Location.where(parent_id: country.id)
+        country_cities.each do |city|
+          options << ["#{region.name} : #{country.name} : #{city.name}", "#{city.id}"]
+        end
+      end
+    end
+    return options
+  end
 end
