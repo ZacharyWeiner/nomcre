0f767ca636ea401ff879004ac4c6c84c6bbe45e8
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2018-03-12 17:38:08 -0600

    chat room query moved to controller, edited show page for shot list item, updated user chat room list, increased  the opacity on home header, added widgets to creative proposal view, fixed layout for message in the top menu

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,6 +3,11 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+    if current_user.user_type == "creative"
+      @proposals = current_user.proposals.where('deadline > ?', Date.today - 14.days )
+    else
+      @proposals = current_user.company.proposals.where('deadline > ?', Date.today - 14.days )
+    end
     notifications = Notification.where(user: current_user, notification_type: "New Message", notification_object_id: params[:id], read:false)
     notifications.each do |note|
       note.read = true
