084ba8891d2a8e1f0b85c0fb3c0e98da07f231d3
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2018-04-02 11:41:52 -0600

    added authorization to admin pages

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,5 @@
 class AdminController < ApplicationController
+  before_action :is_admin
   layout 'adminlte'
   def users
     @users = User.all.page params[:page]
@@ -7,5 +8,11 @@ class AdminController < ApplicationController
 
   def proposals
     @proposals = Proposal.all.page params[:page]
+  end
+
+  def is_admin
+    if current_user.nil? || current_user.role != 0
+      redirect_to root_path
+    end
   end
 end
