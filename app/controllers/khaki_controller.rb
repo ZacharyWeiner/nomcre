4396e6452b0a3a125f55f9b0bc6979683f8a2a90
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2018-12-06 10:36:45 -0600

    removed twilio from about

--- app/controllers/khaki_controller.rb
+++ app/controllers/khaki_controller.rb
@@ -28,16 +28,16 @@ class KhakiController < ApplicationController
   end
 
   def about
-    @twilio_number = ENV['TWILIO_NUMBER']
-    account_sid = ENV['TWILIO_ACCOUNT_SID']
-    @client = Twilio::REST::Client.new account_sid, ENV['TWILIO_CLIENT_ID']
-    time_str = ((Time.now).localtime).strftime("%I:%M%p on %b. %d, %Y")
-    reminder = "Hi Zack. Just a reminder that you have an appointment coming up at #{time_str}."
-    message = @client.api.account.messages.create(
-      :from => @twilio_number,
-      :to => '19546461240',
-      :body => reminder,
-    )
+    # @twilio_number = ENV['TWILIO_NUMBER']
+    # account_sid = ENV['TWILIO_ACCOUNT_SID']
+    # @client = Twilio::REST::Client.new account_sid, ENV['TWILIO_CLIENT_ID']
+    # time_str = ((Time.now).localtime).strftime("%I:%M%p on %b. %d, %Y")
+    # reminder = "Hi Zack. Just a reminder that you have an appointment coming up at #{time_str}."
+    # message = @client.api.account.messages.create(
+    #   :from => @twilio_number,
+    #   :to => '19546461240',
+    #   :body => reminder,
+    # )
   end
 
   def featured_creatives
