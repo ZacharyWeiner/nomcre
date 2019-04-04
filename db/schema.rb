2003fad00ed4be950338a1356d886697e2a3a137
Zack Weiner <zacharyaweiner@gmail.com>
2019-04-04 10:55:21 -0500

    added page type for FAQ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190308135149) do
+ActiveRecord::Schema.define(version: 20190404154915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -273,6 +273,7 @@ ActiveRecord::Schema.define(version: 20190308135149) do
     t.string "slug"
     t.text "description"
     t.string "keywords"
+    t.boolean "is_faq"
   end
 
   create_table "payments", force: :cascade do |t|
