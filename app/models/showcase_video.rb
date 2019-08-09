ffebcae766d9a7392ff0edf30437e47d1c10c63b
Zack Weiner <zacharyaweiner@gmail.com>
2019-08-09 17:06:07 -0400

    removed validation from showcase videos

--- app/models/showcase_video.rb
+++ app/models/showcase_video.rb
@@ -2,6 +2,6 @@ class ShowcaseVideo < ApplicationRecord
   mount_uploader :file, VideoUploader
   mount_uploader :thumbnail, LogoUploader
 
-  validates :file, presence: true,  file_size: { less_than_or_equal_to: 50.megabytes }
-  validates :thumbnail, presence: true, file_size: { less_than_or_equal_to: 20.megabytes }
+  #validates :file, presence: true,  file_size: { less_than_or_equal_to: 50.megabytes }
+  #validates :thumbnail, presence: true, file_size: { less_than_or_equal_to: 20.megabytes }
 end
